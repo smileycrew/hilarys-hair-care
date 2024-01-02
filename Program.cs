0af0d0638c25586c86a4ee2bfa72efe879facc40
Ryan Jones <ryancjonesy@gmail.com>
2024-01-02 14:04:50 -0600

    3rd commit

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
+using HillarysHaircare.Models;
+using HillarysHaircare.Models.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// allows passing datetimes without time zone data 
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+// allows our api endpoints to access the database through Entity Framework Core
+builder.Services.AddNpgsql<HillarysHaircareDbContext>(builder.Configuration["HilarysHairCareConnectionString"]);
+
 
 var app = builder.Build();
 
